5c1efda927da47db37d7e4abbb2360c02131d012
deepanarupalle <narupalle.deepa@capgemini.com>
2022-09-01 14:49:15 +0530

    emsfrontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {path: 'users', component: UserListComponent},
+  {path: 'create-user', component:   CreateUserComponent},
+ 
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'update-user/:id', component:  UpdateUserComponent},
+  {path: 'user-details/:id', component:   UserDetailsComponent},
+
+  // {path: 'vegetables', component: VegetableListComponent},
+  // {path: 'create-vegetable', component:CreateVegetableComponent},
+ 
+  // {path: '', redirectTo: 'vegetables', pathMatch: 'full'},
+  // {path: 'update-vegetable/:id', component: UpdateVegetableComponent},
+  // {path: 'vegetable-details/:id', component: VegetableDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
